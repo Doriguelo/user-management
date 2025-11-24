37ae7b072b7dd21117c02074f900dd809c9bd016
tdoriguelo <tiagodoriga@gmail.com>
2025-11-24 00:51:11 -0300

    feat: create User entity implementing UserDetails and UserRole

---
+++ src/main/java/com/doriguelo/user_management_api/entity/UserRole.java
@@ -0,0 +1,15 @@
+package com.doriguelo.user_management_api.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+}
