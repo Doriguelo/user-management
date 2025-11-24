92cd6ed9a4f44306596cba03fa5cf87f88f0afdb
tdoriguelo <tiagodoriga@gmail.com>
2025-11-24 01:14:09 -0300

    feat: create UserRepository interface with findByLogin method

---
+++ src/main/java/com/doriguelo/user_management_api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.doriguelo.user_management_api.repository;
+
+import com.doriguelo.user_management_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
