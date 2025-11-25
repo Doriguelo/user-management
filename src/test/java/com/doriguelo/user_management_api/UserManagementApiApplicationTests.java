2b6fdcc2da0caaf2ec8a9ca8ad37dbc8d03c6f27
tdoriguelo <tiagodoriga@gmail.com>
2025-11-24 23:23:26 -0300

    fix: remove hardcoded credentials from docker-compose

--- src/test/java/com/doriguelo/user_management_api/UserManagementApiApplicationTests.java
+++ src/test/java/com/doriguelo/user_management_api/UserManagementApiApplicationTests.java
@@ -8,12 +8,12 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false",
 		"spring.datasource.driverClassName=org.h2.Driver",
 		"spring.datasource.username=sa",
 		"spring.datasource.password=",
 		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-		"api.security.token.secret=segredo-de-teste-mockado"
+		"api.security.token.secret=segredo-teste-mock"
 })
 class UserManagementApiApplicationTests {
 
